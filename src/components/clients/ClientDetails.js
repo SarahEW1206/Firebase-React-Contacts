bddeab74682dcb6f2af4c8e710b26025d3c082bd
Sarah Waldie <sarah@sassafrasbiz.com>
2019-02-13 15:38:01 -0500

    Git commit to catch up recent changes/reflect current project progress.

---
+++ src/components/clients/ClientDetails.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import Spinner from "../layout/Spinner";
+
+class ClientDetails extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default ClientDetails;
