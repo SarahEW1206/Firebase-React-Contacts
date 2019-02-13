8e7a7c75fff4e0b9ff7e732210704ea379499999
Sarah Waldie <sarah@sassafrasbiz.com>
2019-02-13 16:33:11 -0500

    Added comments to help myself understand the firestoreConnect and connect functions in compose()!

--- src/components/clients/ClientDetails.js
+++ src/components/clients/ClientDetails.js
@@ -8,8 +8,32 @@ import Spinner from "../layout/Spinner";
 
 class ClientDetails extends Component {
   render() {
-    return <div />;
+    const { client } = this.props;
+
+    if (client) {
+      return (
+        <div>
+          <h1>Details</h1>
+          <h2>{client.firstName}</h2>
+        </div>
+      );
+    } else {
+      return <Spinner />;
+    }
   }
 }
 
-export default ClientDetails;
+export default compose(
+  firestoreConnect(props => [
+    //we already have "clients" from Clients.js, and we only need one here, so we will get the client id from URL and store the respective client data as "client"
+    { collection: "clients", storeAs: "client", doc: props.match.params.id }
+  ]),
+  //Below we are replacing the "state" param with destructuring of state.firestore.ordered, which if broken out would look like this:
+  // { firestore } = state
+  // and then
+  // { ordered } = firestore
+  // So when you use the "ordered" variable, it reflects state.firestore.ordered
+  connect(({ firestore: { ordered } }, props) => ({
+    client: ordered.client && ordered.client[0]
+  }))
+)(ClientDetails);
